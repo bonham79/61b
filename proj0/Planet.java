c4bbe3db36a746482725c2d022b8d1943983f1d3
Minty Salamander <tmbartley@berkeley.edu>
2018-05-29 22:12:07 -0700

    Committed Planet.java

---
+++ proj0/Planet.java
@@ -0,0 +1,87 @@
+public class Planet {
+	public double xxPos, yyPos, xxVel, yyVel, mass;
+	String imgFileName;
+
+	public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
+		/**Initializes a Planet class */
+		this.xxPos = xxPos;
+		this.yyPos = yyPos;
+		this.xxVel = xxVel;
+		this.yyVel = yyVel;
+		this.mass = mass;
+		this.imgFileName = imgFileName;
+	}
+
+	public Planet(Planet P) {
+		/**Constructs identical planet to Planet P */
+		this.xxPos = P.xxPos;
+		this.yyPos = P.yyPos;
+		this.xxVel = P.xxVel;
+		this.yyVel = P.yyVel;
+		this.mass = P.mass;
+		this.imgFileName = P.imgFileName;
+	}
+
+	public double calcDistance(Planet Q) {
+		/**Returns distance between Planet calling method and supplied planet */
+		double x2 = (this.xxPos - Q.xxPos) * (this.xxPos - Q.xxPos);
+		double y2 = (this.yyPos - Q.yyPos) * (this.yyPos - Q.yyPos);
+		return Math.sqrt(x2 + y2);
+	} 
+
+	public double calcForceExertedBy(Planet Q) {
+		/**Calculates force exerted on planet calling method by Planet Q */
+		return 6.67e-11 * this.mass * Q.mass/
+		this.calcDistance(Q)/this.calcDistance(Q);
+	}
+
+	public double calcForceExertedByX(Planet Q) {
+		/**Calculates horizontal force exerted on object 
+		calling method by Planet Q */
+		double xxDist;
+		if (this.xxPos > Q.xxPos) {
+			xxDist = this.xxPos - Q.xxPos;
+		} else {
+			xxDist = Q.xxPos - this.xxPos;
+		}
+		return this.calcForceExertedBy(Q) * xxDist / this.calcDistance(Q);
+	}
+
+	public double calcForceExertedByY(Planet Q) {
+		/**Calculates vertical force exerted on object 
+		calling method by Planet Q */
+		double yyDist;
+		if (this.yyPos > Q.yyPos) {
+			yyDist = this.yyPos - Q.yyPos;
+		} else {
+			yyDist = Q.yyPos - this.yyPos;
+		}
+		return this.calcForceExertedBy(Q) * yyDist / this.calcDistance(Q);
+	}
+
+	public double calcNetForceExertedByX(Planet[] planets) {
+		/**Calculates the net horizontal force exerted on object calling method
+		by all planets in array planets */
+		double xxNet = 0.0;
+		for (Planet p : planets) {
+			if (this.equals(p)) {
+				continue;
+			}
+			xxNet += this.calcForceExertedByX(p);
+		}
+		return xxNet;
+	}
+
+	public double calcNetForceExertedByY(Planet[] planets) {
+		/**Calculates the net vertical force exerted on object calling method
+		by all planets in array planets */
+		double yyNet = 0.0;
+		for (Planet p : planets) {
+			if (this.equals(p)) {
+				continue;
+			}
+			yyNet += this.calcForceExertedByY(p);
+		}
+		return yyNet;
+	}
+}
