0b82e18e5eba2edfcf004e98ebb9359ef792f3be
Minty Salamander <tmbartley@berkeley.edu>
2018-07-15 14:09:18 -0700

    Made a better test case for ComplexOomage

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -40,16 +40,37 @@ public class TestComplexOomage {
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-        for (int i = 0; i < 100; ++i) {
-            List<Integer> params = new ArrayList<>();
-            for (int j = 0; j < 50; ++j) {
-                params.add(255);
-            }
-            deadlyList.add(new ComplexOomage(params));
-        }
-
         // Your code here.
+        List<Integer> params1 = new ArrayList<>();
+        params1.add(1);//hash = 1;
+        params1.add(0);//hash = 256;
+        params1.add(0);//hash = 256^2
+        params1.add(0);//hash = 256^3
+        params1.add(0);//hash = 256^4
+        ComplexOomage s1 = new ComplexOomage(params1);
 
+        List<Integer> params2 = new ArrayList<>();
+        params2.add(0);
+        params2.add(1);
+        params2.add(0);
+        params2.add(0);
+        params2.add(0);
+        params2.add(0);
+        ComplexOomage s2 = new ComplexOomage(params2);
+
+        List<Integer> params3 = new ArrayList<>();
+        params3.add(0);
+        params3.add(0);
+        params3.add(1);
+        params3.add(0);
+        params3.add(0);
+        params3.add(0);
+        params3.add(0);
+        ComplexOomage s3 = new ComplexOomage(params3);
+
+        deadlyList.add(s1);
+        deadlyList.add(s2);
+        deadlyList.add(s3);
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
     }
 
